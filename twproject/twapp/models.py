2bf39d80e0543f628a8a1e5a2972c9fc5f1aaed0
bFraley <bFraley@users.noreply.github.com>
2016-09-27 14:39:35 -0400

    progress on Member and Tweet models

--- twproject/twapp/models.py
+++ twproject/twapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
-# Create your models here.
+# Define Member and Tweet data models.
+
+class Member(models.Model):
+    username = models.CharField(max_length=20)
+    email = models.TextField(max_length=200)
+
+    def __str__(self):
+        return '{} {}'.format(self.name, self.email)
+
+
+class Tweet(models.Model):
+    msg = models.TextField(max_length=140)
+    date = " date "
+    tags = ["#tag1"]
+
+    def __str__(self):
+        return '{}    {}    {}'.format(self.msg, self.date, self.tags)
