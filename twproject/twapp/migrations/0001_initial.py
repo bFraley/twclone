903185a2b3e835ad4f0ca8665a0cbdc4413179a0
bFraley <bFraley@users.noreply.github.com>
2016-10-03 01:01:42 -0400

    inherit member.user from User

--- twproject/twapp/migrations/0001_initial.py
+++ twproject/twapp/migrations/0001_initial.py
@@ -14,15 +14,19 @@ class Migration(migrations.Migration):
             name='Member',
             fields=[
                 ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
-                ('username', models.CharField(max_length=20)),
-                ('email', models.TextField(max_length=200)),
+                ('username', models.CharField(max_length=50)),
+                ('email', models.EmailField(max_length=254)),
             ],
+            options={
+                'ordering': ['username'],
+            },
         ),
         migrations.CreateModel(
             name='Tweet',
             fields=[
                 ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
                 ('msg', models.TextField(max_length=140)),
+                ('memb', models.ForeignKey(null=True, to='twapp.Member')),
             ],
         ),
     ]
